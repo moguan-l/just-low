1348f53ef890fff57e875d03851ab1eb9d87c975
liqingjie <1530212104@qq.com>
2016-01-26 11:47:22 +0800

    调整样式。

---
+++ web/public/js/filter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liqingjie on 16/1/26.
+ */
+JustLow
+    .filter('decodeURI', function() {
+        return function(input) {
+            return decodeURI(input);
+        }
+    });
